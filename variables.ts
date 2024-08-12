2de36380cecfea9a4cdc8ef14093213e3c0a2afc
James McDonald <jamesmcdonald112@yahoo.ie>
2024-08-12 22:23:18 +0100

    Installed and inatilised TypeScript and node for the project, compiled and tested a file. Everything seems to be working

---
+++ variables.ts
@@ -0,0 +1,15 @@
+// Reassiging a variable to a value of a different type does not work in Typescript
+let order = 'first';
+// order = 1; this will cause an error.
+
+
+// ANY 
+// If a variable is declared without being assigned an initial value, TypeScript will consider this variable to be of type any
+
+// This means it can be assigned and reassigned any value and TypeScript won't give an error if the types do not match later on.
+let onOrOff;
+
+onOrOff = 1;
+onOrOff = false;
+
+console.log("Working");
